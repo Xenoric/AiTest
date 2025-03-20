83d48f1dfc31624779f5afefc29bd7bcac48385f
Xenoric <93491757+Xenoric@users.noreply.github.com>
2025-03-21 01:29:31 +0500

    Old scripts deleted 21.03

--- Assets/Scripts/IBot.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public interface IBot
-{
-    float MoveSpeed { get; set; }
-    float WaypointThreshold { get; set; }
-    Vector2 TargetPosition { get; set; }
-   void UpdateBot();
-}
--- Assets/Scripts/Utils/NodeNeighborsData.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-
-[Serializable]
-public class NodeNeighborsData
-{
-    public NodeData currentNode;
-    public List<NodeData> neighborNodes = new List<NodeData>();
-}
